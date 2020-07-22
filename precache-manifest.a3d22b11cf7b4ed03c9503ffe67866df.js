1baa3ac3494a0210980f52ef6ba9a69eea3964f8
y0092888 <ju-horn@web.de>
2020-07-22 10:43:01 +0200

    Updates

---
+++ precache-manifest.a3d22b11cf7b4ed03c9503ffe67866df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b52fd25dbb1b79dfa5347ade822937",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "bb9cbb8f856f720bf422",
+    "url": "/todo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8148acb5bfc0607d5f8",
+    "url": "/todo/static/js/2.471bf600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.471bf600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb9cbb8f856f720bf422",
+    "url": "/todo/static/js/main.da02355a.chunk.js"
+  },
+  {
+    "revision": "10fd3631c43d335a3db9",
+    "url": "/todo/static/js/runtime-main.2a8ec137.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
