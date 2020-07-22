ea7a3d42161100e76c64a7f3f39e68bd0a5bc6a6
y0092888 <ju-horn@web.de>
2020-07-22 17:05:02 +0200

    refactor and added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { shallow, mount } from "enzyme";
+
+describe("App", () => {
+  const app = mount(<App />);
+  test("exist", () => {
+    expect(app.find(".create > input").exists()).toBe(true);
+    expect(app.find(".create > button").exists()).toBe(true);
+    expect(
+      app
+        .findWhere((n) => {
+          return n.prop("done") === false;
+        })
+        .exists()
+    ).toBe(true);
+    expect(
+      app
+        .findWhere((n) => {
+          return n.prop("done") === true;
+        })
+        .exists()
+    ).toBe(true);
+  });
+
+  test("create new todo", () => {
+    expect(app.find("Todo").exists()).toBe(true);
+
+    app
+      .find(".create > input")
+      .simulate("change", { target: { value: "new todo" } });
+
+    app.find(".create > button").simulate("click");
+    expect(app.find("Todo").exists()).toBe(true);
+  });
 });
