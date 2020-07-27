b2391e436f17b93e6f15d58b5cba5bdff4ba9cd7
y0092888 <ju-horn@web.de>
2020-07-27 14:24:03 +0200

    improved style

--- src/component/Confirm.js
+++ src/component/Confirm.js
@@ -2,23 +2,32 @@ import React from "react";
 
 import { confirmAlert } from "react-confirm-alert"; // Import
 import "react-confirm-alert/src/react-confirm-alert.css"; // Import css
+import styles from "./confirm.module.css";
 
 export default (h1, p, onYes) => {
   confirmAlert({
     customUI: ({ onClose }) => {
       return (
-        <div className="custom-ui" style={{ backgroundColor: "green" }}>
+        <div className="custom-ui">
           <h1>{h1}</h1>
           <p>{p}</p>
-          <button onClick={onClose}>No</button>
-          <button
-            onClick={() => {
-              onYes();
-              onClose();
-            }}
-          >
-            Yes!
-          </button>
+          <form className={styles.form}>
+            <button
+              className={styles.button + " " + styles.safe}
+              onClick={onClose}
+            >
+              No
+            </button>
+            <button
+              className={styles.button + " " + styles.danger}
+              onClick={() => {
+                onYes();
+                onClose();
+              }}
+            >
+              Yes
+            </button>
+          </form>
         </div>
       );
     },
